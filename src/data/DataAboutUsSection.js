e3b29e39923f46a2dec0a62f65f18fe31cdc0502
Mohamed Ali Zbaira <mohamedalizbaira@gmail.com>
2025-11-03 22:25:35 +0100

    update dataAbout and delete  vite svg

--- src/data/DataAboutUsSection.js
+++ src/data/DataAboutUsSection.js
@@ -3,8 +3,8 @@
 export const ceoImageUrl = "/images/profile.jpg";
 
 export const aboutUsText = [
-  "Empire Properties, established in November 2023, specializes in comprehensive real estate services, including property management, rentals, and sales. We offer expertise in both ready and off-plan properties, serving clients across Ras Al Khaimah and Dubai with a commitment to excellence and tailored solutions for all your property needs.",
-  "Mohamed Ali Ben Gaied is the CEO and founder of Empire Properties FZ LLC. With 10 years of experience in the real estate industry, he specializes in selling premium properties in Ras Al Khaimah and Dubai. His mission is to provide exceptional service, helping clients find their ideal homes and investment opportunities. At Empire Properties, he is committed to excellence and innovation in the UAE real estate market."
+  "Skyline Estates, established in November 2023, specializes in comprehensive real estate services, including property management, rentals, and sales. The agency provides expertise in both ready-to-move-in and off-plan properties, serving clients across Ras Al Khaimah and Dubai.",
+  "Skyline Estates is dedicated to helping clients find their ideal homes and investment opportunities. With a focus on personalized service and market knowledge, the agency ensures smooth transactions and professional guidance at every step. Committed to excellence and innovation, Skyline Estates strives to make every real estate experience seamless and rewarding for its clients."
 ];
 
 export const stats = [
